a50dd2e3219025a88b18596ccd489366e9c3eb9c
iot-emdedded <nguyenquan5895@gmail.com>
2018-08-19 10:38:19 +0700

    [19/8] update eeprom.h

--- NodeESP8266/eeprom.h
+++ NodeESP8266/eeprom.h
@@ -62,7 +62,7 @@ String ReadStringFromEEPROM(int address)
  *             +address : address in EEPROM
  * Return: None.
  */
-void SaveIntToEEPROM(int data,int address) {
+void SaveCharToEEPROM(char data,int address) {
   EEPROM.write(address,data); 
   EEPROM.commit();
 }
@@ -72,6 +72,49 @@ void SaveIntToEEPROM(int data,int address) {
  * Parameter : +address : address in EEPROM
  * Return: int.
  */
-int ReadIntFromEEPROM(int address) {
+int ReadCharFromEEPROM(char address) {
   return EEPROM.read(address);
+}
+
+/*
+ * Function Save Long To EEPROM
+ * Parameter : +data    : int Data
+ *             +address : address in EEPROM
+ * Return: None.
+ */
+void SaveLongToEEPROM(long data,int address) {
+  char byte1 = (data >> 24) & 0xFF;
+  char byte2 = (data >> 16) & 0xFF;
+  char byte3 = (data >> 8) & 0xFF;
+  char byte4 = data & 0xFF;
+  // Serial.println("Write Long:");
+  // Serial.println("Byte1:" + String(byte1, HEX));
+  // Serial.println("Byte2:" + String(byte2, HEX));
+  // Serial.println("Byte3:" + String(byte3, HEX));
+  // Serial.println("Byte4:" + String(byte4, HEX));
+  EEPROM.write(address, byte1); 
+  EEPROM.write(address+1, byte2); 
+  EEPROM.write(address+2, byte3); 
+  EEPROM.write(address+3, byte4); 
+  EEPROM.commit();
+}
+
+/*
+ * Function Read Long From EEPROM
+ * Parameter : +address : address in EEPROM
+ * Return: int.
+ */
+long ReadLongFromEEPROM(long address) {
+  long byte1 = EEPROM.read(address);
+  long byte2 = EEPROM.read(address + 1);
+  long byte3 = EEPROM.read(address + 2);
+  long byte4 = EEPROM.read(address + 3);
+  // Serial.println("Read Long:");
+  // Serial.println("Byte1:" + String(byte1, HEX));
+  // Serial.println("Byte2:" + String(byte2, HEX));
+  // Serial.println("Byte3:" + String(byte3, HEX));
+  // Serial.println("Byte4:" + String(byte4, HEX));
+  long result = ((byte1 << 24) & 0xFFFFFFFF)  + ((byte2 << 16 ) & 0xFFFFFF)  + ((byte3 << 8) & 0xFFFF)  + (byte4 & 0xFF);
+  // Serial.println("Read Long:" + String(result));
+  return result;
 }
